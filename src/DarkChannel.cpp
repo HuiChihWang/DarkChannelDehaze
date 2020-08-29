0de9f669d9ecba0474fdb47136c1590143c40daf
Gilbert <taya87136@gmail.com>
2020-08-29 13:53:03 +0800

    add dark channel prototype

--- src/DarkChannel.cpp
+++ src/DarkChannel.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "DarkChannel.h"
+#include "opencv2/imgproc.hpp"
+
+void CDarkChannel::SetImage(const cv::Mat &matImage) {
+    m_matImage = matImage;
+}
+
+void CDarkChannel::SetParameter(TDarkChannelSetting* pTSetting) {
+    m_pTSetting = pTSetting;
+}
+
+bool CDarkChannel::CalDarkChannel() {
+    if (m_matImage.empty()) {
+        return false;
+    }
+
+    auto matRGBMin = FilterRGBMin(m_matImage);
+    auto matDarkChannelRough = FilterGrayMin(matRGBMin, cv::Size());
+
+    m_matDarkChannel = matDarkChannelRough;
+    return true;
+}
+
+const cv::Mat &CDarkChannel::GetDarkChannel() {
+    return m_matDarkChannel;
+}
+
+void CDarkChannel::ClearResult() {
+    m_matDarkChannel.release();
+    m_matImage.release();
+}
+
+CDarkChannel::~CDarkChannel() {
+    ClearResult();
+}
+
+cv::Mat CDarkChannel::FilterRGBMin(const cv::Mat &matImage) {
+
+    std::vector<cv::Mat> vecChannels{3};
+    cv::split(matImage, vecChannels);
+
+    return cv::Mat(cv::min(vecChannels[0], cv::min(vecChannels[1], vecChannels[2])));
+}
+
+cv::Mat CDarkChannel::FilterGrayMin(const cv::Mat &matImage, const cv::Size sizePatch) {
+    cv::Mat matGrayMin;
+    auto matKernel = cv::getStructuringElement(cv::MORPH_RECT, sizePatch);
+    cv::erode(matImage, matGrayMin, matKernel);
+
+    return matGrayMin;
+}
+
--- src/DarkChannel.h
+++ src/DarkChannel.h
@@ -1,13 +1,39 @@
 //
 // Created by Hui Chih Wang on 2020/8/28.
 //
-
 #ifndef DEHAZE_DARKCHANNEL_H
 #define DEHAZE_DARKCHANNEL_H
 
+#include "opencv2/core.hpp"
 
-class DarkChannel {
 
+struct TDarkChannelSetting {
+    cv::Size sizePatch = cv::Size{7,7};
+
+};
+
+class CDarkChannel {
+
+public:
+    CDarkChannel() = default;
+    ~CDarkChannel();
+
+    void SetImage(const cv::Mat& matImage);
+    void SetParameter(TDarkChannelSetting* pTSetting);
+
+    bool CalDarkChannel();
+    const cv::Mat& GetDarkChannel();
+
+
+    void ClearResult();
+
+private:
+    static cv::Mat FilterRGBMin(const cv::Mat& matImage);
+    static cv::Mat FilterGrayMin(const cv::Mat &matImage, const cv::Size sizePatch);
+
+    cv::Mat m_matImage;
+    cv::Mat m_matDarkChannel;
+    TDarkChannelSetting* m_pTSetting = new TDarkChannelSetting;
 };
 
 
